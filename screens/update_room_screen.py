52df624c9adde76a05c9faea15cff448eb793d15
jcr04 <joaocarlosreboucas15@gmail.com>
2023-09-30 20:06:30 -0300

    add news features

--- screens/update_room_screen.py
+++ screens/update_room_screen.py
@@ -4,6 +4,8 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
 from kivy.uix.textinput import TextInput
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
 import requests
 
 
@@ -20,24 +22,36 @@ class UpdateRoomScreen(Screen):
 
     def _create_layout(self):
         layout = BoxLayout(orientation='vertical', padding=15, spacing=10)
-        
+
+    # Criar o Dropdown antes de vinculá-lo ao botão
+        self.room_dropdown = DropDown(auto_dismiss=False)
+        self.populate_room_dropdown()
+
+    # Criar um layout superior para acomodar o título e o botão de seleção de sala
+        top_layout = BoxLayout(orientation='horizontal', spacing=10)
+        top_layout.add_widget(Label(text="Atualizar sala", size_hint_x=None, width=150))
+
+    # Botão de seleção de sala
+        select_button = Button(text='Selecione uma Sala', size_hint=(None, None), height=50)
+        select_button.bind(on_release=self.room_dropdown.open)
+        top_layout.add_widget(select_button)
+
+        layout.add_widget(top_layout)  # Adiciona o top_layout ao layout principal
+
+    # Inicializando os TextInput
         self.name_input = TextInput(hint_text="Nome da sala", multiline=False)
         self.capacity_input = TextInput(hint_text="Capacidade", multiline=False)
         self.description_input = TextInput(hint_text="Descrição", multiline=False)
-        
-        elements = [
-            Label(text="Atualizar sala"),
-            self.name_input,
-            self.capacity_input,
-            self.description_input,
-            self._create_update_button(),
-            self._create_back_button()
-        ]
-        
-        for element in elements:
-            layout.add_widget(element)
-        
+
+    # Adicionando os demais widgets ao layout principal
+        layout.add_widget(self.name_input)
+        layout.add_widget(self.capacity_input)
+        layout.add_widget(self.description_input)
+        layout.add_widget(self._create_update_button())
+        layout.add_widget(self._create_back_button())
+
         return layout
+
     
     def _set_background(self):
         with self.canvas:
@@ -58,6 +72,25 @@ class UpdateRoomScreen(Screen):
     def update_background(self, instance, value):
         self.background.pos = self.pos
         self.background.size = self.size
+        
+    def populate_room_dropdown(self):
+        rooms_url = 'http://127.0.0.1:5001/api/rooms'
+        response = requests.get(rooms_url)
+
+        if response.status_code == 200:
+            rooms = response.json()
+        for room in rooms:
+            btn = Button(text=str(room['id']), size_hint_y=None, height=44)
+            btn.bind(on_release=lambda btn: self.on_room_select(btn.text))
+            self.room_dropdown.add_widget(btn)
+        else:
+            print(f'Error fetching rooms: {response.status_code}')
+
+    def on_room_select(self, room_id):
+        self.room_id = room_id
+        self.room_dropdown.dismiss()
+        print(f'Selected Room ID: {self.room_id}')
+
 
     def update_room(self, instance):
         url = f'http://127.0.0.1:5001/api/rooms/{self.room_id}/update'
